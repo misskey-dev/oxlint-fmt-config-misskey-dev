8ebb153ffac11cbd374104af521461279e5b7e85
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-06-07 21:36:50 +0900

    fix

--- src/oxlint/index.ts
+++ src/oxlint/index.ts
@@ -38,9 +38,9 @@ function defineMisskeyDevOxlintConfig(opts: MisskeyDevOxlintConfigOptions = {}):
 }
 
 export { defineMisskeyDevOxlintConfig };
-export { jsRules } from './javascript.js';
-export { tsRules } from './typescript.js';
-export { stylisticRules } from './stylistic.js';
-export { corsaStylisticRules } from './corsa-stylistic.js';
+export { jsRules as oxlintJsRules } from './javascript.js';
+export { tsRules as oxlintTsRules } from './typescript.js';
+export { stylisticRules as oxlintStylisticRules } from './stylistic.js';
+export { corsaStylisticRules as oxlintCorsaStylisticRules } from './corsa-stylistic.js';
 
 export type { MisskeyDevOxlintConfigOptions };
